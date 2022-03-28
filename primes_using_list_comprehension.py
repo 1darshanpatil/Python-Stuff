54d449e0884c31e24f677e1d6bb3705c1d211c31
Darshan P <72539638+1darshanpatil@users.noreply.github.com>
2022-03-28 20:47:54 +0530

    Update primes_using_list_comprehension.py

--- primes_using_list_comprehension.py
+++ primes_using_list_comprehension.py
@@ -1,7 +1,7 @@
 x = int(input("Enter Int > 2: "))
-primes_using_all = list(filter(lambda N: all(list(N%n for n in range(2, x))), range(2, 100)))
+primes_using_all = list(filter(lambda N: all(list(N%n for n in range(2, N))), range(2, x)))
 primes_using_double_loop = list(N for N in range(2, x) if all(N%n for n in range(2, N)))
 
 import sympy
 primes_using_sympy = list(N for N in range(2, x) if sympy.isprime(N))
-print(primes_using_all); print(primes_using_doubl_loop); print(primes_using_sympy)
+print(primes_using_all); print(primes_using_double_loop); print(primes_using_sympy)
